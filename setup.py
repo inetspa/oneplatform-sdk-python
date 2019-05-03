b8609c41dfa86f7f937540ba6406023bb36bf486
Patcharapong Prohmwichai <patcharp@live.com>
2019-05-03 18:14:48 +0700

    Update new setup.py and release version 0.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-__version__ = ''
-
-
-def _requirements():
-    with open('requirements.txt', 'r') as fd:
-        return [name.strip() for name in fd.readlines()]
+__version__ = '0.1.0'
 
 
 def _long_description():
@@ -15,7 +10,7 @@ def _long_description():
 
 
 setup(
-    name="oneplatform-sdk",
+    name="pyoneplatform",
     version=__version__,
     author="INET-SDI",
     author_email="inet-sdi@inet.co.th",
@@ -26,17 +21,10 @@ setup(
     long_description=_long_description(),
     license='GNU GPL 3.0',
     packages=[
-        "oneid"
+        "oneplatform"
     ],
-    install_requires=_requirements(),
-    classifiers=[
-        "License :: OSI Approved :: GNU GPL v.3.0 Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development"
-    ]
+    install_requires=[
+        "requests",
+        "urllib3"
+    ],
 )
